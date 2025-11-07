3b8da3d7169f1f6124b9bdf391421ee97d7eee67
Mohd Kabeer Mansoori <mohdkabeer.dev@gmail.com>
2025-11-07 20:21:54 +0530

    resolve issue

--- Components/Section4.jsx
+++ Components/Section4.jsx
@@ -6,7 +6,7 @@ function Section4() {
   return (
     <div className='section4 flex'>
       <div className='aboutUs flex'>
-  <AboutUs />
+        <AboutUs />
       </div>
       <div className='aboutImg'><img src="public/section4_img1.jpg" alt="" width={100} /></div>
     </div>
